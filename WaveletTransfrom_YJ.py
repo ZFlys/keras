f6ab0351159bac3f99819f9c88766dbd118a6f7a
Msword9465 <wustxyj@gmail.com>
2018-12-07 11:03:45 +0800

    test

--- WaveletTransfrom_YJ.py
+++ WaveletTransfrom_YJ.py
@@ -9,7 +9,7 @@ import numpy as np
 import pandas as pd
 import pywt
 import matplotlib.pyplot as plt
-import osn
+import os
 plt.rcParams['font.sans-serif'] = ['SimHei']
 plt.rcParams['axes.unicode_minus'] = False
 
@@ -31,26 +31,32 @@ def wpt1D(data,wavelet='db1',mode='symmetric',maxlevel=2):
     return df_wpt
 
 
-df_yes = pd.read_table(r'D:\Jupyter Notebook\Research_Ultrasound\质量检测\yes_no\yes.zwav',
-                       engine='python',sep='\t').set_index('TIME(us)')
-df_no = pd.read_table(r'D:\Jupyter Notebook\Research_Ultrasound\质量检测\yes_no\no.zwav',
-                    engine='python',sep='\t').set_index('TIME(us)')
-
-
-df = pd.read_pickle('ut_example.pkl')
-df.columns = ['DATA']
-
-def plot_wpt(df1, df2,maxlevel, title):
+# df_yes = pd.read_table(r'D:\Jupyter Notebook\Research_Ultrasound\质量检测\yes_no\yes.zwav',
+#                        engine='python',sep='\t').set_index('TIME(us)')
+# df_no = pd.read_table(r'D:\Jupyter Notebook\Research_Ultrasound\质量检测\yes_no\no.zwav',
+#                     engine='python',sep='\t').set_index('TIME(us)')
+# df = pd.read_pickle('ut_example.pkl')
+# df.columns = ['DATA']
     
-    df_wpt1 = wpt1D(df1[65:75.22]['DATA'],'db1','symmetric',maxlevel)
-    df_wpt2 = wpt1D(df2[65:75.22]['DATA'],'db1','symmetric',maxlevel)
+df1 = pd.read_csv('WPT/HDPE_WOOD_0.csv',engine='python',header=None)
+df2 = pd.read_csv('WPT/HDPE_WOOD_1.csv',engine='python',header=None)
+df3 = pd.read_csv('WPT/HDPE_WOOD_2.csv',engine='python',header=None)
+df1.index = df2.index = df3.index = np.linspace(0,163.8,4096)
+
+
+def plot_wpt(df1, df2, df3, maxlevel, title):
+    
+    df_wpt1 = wpt1D(df1[70:90.44][0],'db1','symmetric',maxlevel)
+    df_wpt2 = wpt1D(df2[70:90.44][0],'db1','symmetric',maxlevel)
+    df_wpt3 = wpt1D(df3[70:90.44][0],'db1','symmetric',maxlevel)
     
     if maxlevel == 1:
         plt.figure()
         for i in  range(1,2**maxlevel+1):
             plt.subplot(2,1,i)
-            plt.plot(df_wpt1.index, df_wpt1.iloc[:,i-1],df_wpt2.iloc[:,i-1])
-            #plt.ylim((-35,35))
+            plt.plot(df_wpt1.index, df_wpt1.iloc[:,i-1])
+            plt.plot(df_wpt2.index, df_wpt2.iloc[:,i-1])  
+            plt.plot(df_wpt3.index, df_wpt3.iloc[:,i-1])            #plt.ylim((-35,35))
             plt.title('{}'.format(df_wpt1.columns[i-1]))
         plt.subplots_adjust(left=None, bottom=None, right=None, top=None, 
         wspace=0.2, hspace=0.4)
@@ -61,8 +67,9 @@ def plot_wpt(df1, df2,maxlevel, title):
         plt.figure()
         for i in  range(1,2**maxlevel+1):
             plt.subplot(2,2,i)
-            plt.plot(df_wpt1.index, df_wpt1.iloc[:,i-1],df_wpt2.iloc[:,i-1])
-            #plt.ylim((-35,35))
+            plt.plot(df_wpt1.index, df_wpt1.iloc[:,i-1])
+            plt.plot(df_wpt2.index, df_wpt2.iloc[:,i-1])  
+            plt.plot(df_wpt3.index, df_wpt3.iloc[:,i-1])            #plt.ylim((-35,35))
             plt.title('{}'.format(df_wpt1.columns[i-1]))
         plt.subplots_adjust(left=None, bottom=None, right=None, top=None, 
         wspace=0.2, hspace=0.4)
@@ -73,8 +80,9 @@ def plot_wpt(df1, df2,maxlevel, title):
         plt.figure()
         for i in  range(1,2**maxlevel+1):    
             plt.subplot(4,2,i)
-            plt.plot(df_wpt1.index, df_wpt1.iloc[:,i-1],df_wpt2.iloc[:,i-1])
-            #plt.ylim((-30,30))
+            plt.plot(df_wpt1.index, df_wpt1.iloc[:,i-1])
+            plt.plot(df_wpt2.index, df_wpt2.iloc[:,i-1])  
+            plt.plot(df_wpt3.index, df_wpt3.iloc[:,i-1])            #plt.ylim((-30,30))
             plt.title('{}'.format(df_wpt1.columns[i-1]))
         plt.subplots_adjust(left=None, bottom=None, right=None, top=None, 
         wspace=0.4, hspace=0.6)
@@ -85,7 +93,9 @@ def plot_wpt(df1, df2,maxlevel, title):
         plt.figure()
         for i in  range(1,2**maxlevel+1):
             plt.subplot(4,4,i)
-            plt.plot(df_wpt1.index, df_wpt1.iloc[:,i-1],df_wpt2.iloc[:,i-1])           
+            plt.plot(df_wpt1.index, df_wpt1.iloc[:,i-1])
+            plt.plot(df_wpt2.index, df_wpt2.iloc[:,i-1])  
+            plt.plot(df_wpt3.index, df_wpt3.iloc[:,i-1])
             #plt.ylim((-35,35))
             plt.title('{}'.format(df_wpt1.columns[i-1]))
         plt.subplots_adjust(left=None, bottom=None, right=None, top=None, 
@@ -94,8 +104,10 @@ def plot_wpt(df1, df2,maxlevel, title):
         plt.show()
     
     
-pd.concat([df_yes['DATA'],df_no['DATA']],axis=1)[65:75.22].plot()
-plot_wpt(df_yes,df_no,4,'合格与不合格')
+HDPE_WOOD = pd.concat([df1,df2,df3],axis=1)[70:90.44]
+HDPE_WOOD.columns = [0,1,2]
+HDPE_WOOD.plot()
+plot_wpt(df1,df2,df3,4,'合格与不合格')
 
 # =============================================================================
 # #导入示例数据测试(DF结构，索引为Time，列为幅值)
